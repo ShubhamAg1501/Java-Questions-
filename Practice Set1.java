9d9ec7b2d80015f7a33fda9418f761de13b72311
Shubham <shubhamagrawal1501@gmail.com>
2024-01-17 00:37:43 +0530

    First Commit

---
+++ Practice Set1.java
@@ -0,0 +1,36 @@
+package com.company;
+public class Practice_Set_1{
+    public static void main(String[] args) {
+//        Question1
+//        int x = 4;
+//        int y = 17;
+//        int z =6;
+//        int sum = x+y+z;
+//        System.out.println(sum);
+
+//        Question2
+//    float subject1 = 45;
+//    float subject2 = 95;
+//    float subject3 = 48;
+//    float cgpa = (subject1 + subject2 +subject3)/30;
+//    System.out.println(cgpa);
+
+//        Question 3
+//        System.out.println("What is your name");
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.next();
+//        System.out.println("Hello " + name + " have a good day!");
+
+//        Question 4
+//          System.out.println("Enter the number of kilometres");
+//          Scanner sc = new Scanner(System.in);
+//          int km = sc.nextInt();
+//          km = km * 0.621372
+//          System.out.println("Updated value in miles "+"= "+km);
+
+//        Question 5
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
